4118db30874b1faa4df59b5565c6c84d00277333
Salvador Guzman <guzmansalv@gmail.com>
2019-05-20 17:53:35 -0700

    http request

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,30 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
-const URL: &'static str = "postgresql://admin@localhost:5432/youtube";
-const COUNT: &'static str = "SELECT COUNT(*) FROM youtube.stats.channels";
+const POSTGRESQL_URL: &'static str = "postgresql://admin@localhost:5432/youtube";
+const INSERT: &'static str =
+    "INSERT INTO youtube.stats.channels (serial) VALUES ($1) ON CONFLICT DO NOTHING";
+const PAGE_LIMIT: u32 = 114462;
+const RANGE_LIMIT: u32 = PAGE_LIMIT + 1;
 
 struct Channel {
     id: u32,
-    serial: [char; 24]
+    serial: String
 }
 
 fn main() {
-    let params: &'static str = URL;
+    /*let params: &'static str = POSTGRESQL_URL;
     let tls: postgres::TlsMode = postgres::TlsMode::None;
 
     let conn: postgres::Connection =
-        postgres::Connection::connect(params, tls).unwrap();
+        postgres::Connection::connect(params, tls).unwrap();*/
 
-    let query: &'static str = COUNT;
-    let row: postgres::rows::Rows = conn.query(query, &[]).unwrap();
-    let count: i64 = row.get(0).get(0);
-    println!("Count is {}", count);
+    for i in 1..RANGE_LIMIT {
+        let url: String =
+            format!("http://www.channelcrawler.com/eng/results/136614/page:{}", i);
+        let body: String = reqwest::get(url.as_str()).unwrap().text().unwrap();
+        println!("{}", body);
+    }
 
     println!("Hello, world!");
 }
