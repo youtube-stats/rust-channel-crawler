08947f979acac46c522a7cc9fcacff1b720a5762
Salvador Guzman <guzmansalv@gmail.com>
2019-05-20 17:12:40 -0700

    db main stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate postgres;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+
+const URL: &'static str = "postgresql://admin@localhost:5432/youtube";
+const COUNT: &'static str = "SELECT COUNT(*) FROM youtube.stats.channels";
+
+struct Channel {
+    id: u32,
+    serial: [char; 24]
+}
+
 fn main() {
+    let params: &'static str = URL;
+    let tls: postgres::TlsMode = postgres::TlsMode::None;
+
+    let conn: postgres::Connection =
+        postgres::Connection::connect(params, tls).unwrap();
+
+    let query: &'static str = COUNT;
+    let row: postgres::rows::Rows = conn.query(query, &[]).unwrap();
+    let count: i64 = row.get(0).get(0);
+    println!("Count is {}", count);
+
     println!("Hello, world!");
 }
